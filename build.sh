e612bfc62903b934e28a9919af5119220cb5e6af
Joshua Sierles <joshua@diluvia.net>
2017-01-06 19:57:01 +0100

    add python3-mini dockerfile

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 #
 
-docker build -t nextjournal/python2 -f Dockerfile.python2 .
-docker build -t nextjournal/python3 -f Dockerfile.python3 .
+docker build -t nextjournal/python2 -f Dockerfile.python2 . &
+docker build -t nextjournal/python3 -f Dockerfile.python3 . &
+docker build -t nextjournal/python3-mini -f Dockerfile.python3-mini .
