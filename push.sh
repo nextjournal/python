a4e867cdbdc9b1b854587a6a7fdc3807e61633f1
Joshua Sierles <joshua@diluvia.net>
2017-01-06 19:56:40 +0100

    fix push script

--- push.sh
+++ push.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 #
-TAG=$(git branch)-$(git rev-parse --short HEAD)
+TAG=$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short HEAD)
 echo "Tagging as $TAG"
-git tag $(git branch)-$(git rev-parse --short HEAD)
+git tag $TAG
 echo "Pushing to trigger automated build"
 git push --tags
+git push
 echo "Check builds at these URLs"
-echo "https://hub.docker.com/r/nextjournal/python2-automated/builds/"
-echo "https://hub.docker.com/r/nextjournal/python3-automated/builds/"
+echo "https://hub.docker.com/r/nextjournal/python2/builds/"
+echo "https://hub.docker.com/r/nextjournal/python3/builds/"
