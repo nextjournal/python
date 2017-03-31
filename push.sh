49998ed8c13dbc9334b7557ce28673881481f5d8
Joshua Sierles <joshua@diluvia.net>
2017-03-31 19:25:29 +0200

    push to GCR as well

--- push.sh
+++ push.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 #
 TAG=$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short HEAD)
-echo "Tagging as $TAG"
+GCR_IMAGE=eu.gcr.io/nextjournal-com/python:$TAG
+docker build -t nextjournal/python:$TAG .
+docker tag nextjournal/python:$TAG $GCR_IMAGE
+echo "Tagging $TAG"
 git tag $TAG
-echo "Pushing to trigger automated build"
+echo "Pushing to $GCR_IMAGE..."
+docker push $GCR_IMAGE
+echo "Pushing to Github, which triggers an automated build for nextjournal/python:$TAG"
 git push --tags
 git push
-echo "Check builds at these URLs"
-echo "https://hub.docker.com/r/nextjournal/python2/builds/"
-echo "https://hub.docker.com/r/nextjournal/python3/builds/"
