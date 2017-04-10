dcc2ebdd6457d1b3a7e32b73545828a25087bef4
Joshua Sierles <joshua@diluvia.net>
2017-04-10 19:12:22 +0200

    split build and push scripts

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@
 TAG=$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short HEAD)
 docker pull nextjournal/python:latest
 docker build --cache-from nextjournal/python:latest -t nextjournal/python:latest .
+docker tag nextjournal/python:latest nextjournal/python:$TAG
--- push.sh
+++ push.sh
@@ -2,12 +2,14 @@
 #
 TAG=$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short HEAD)
 GCR_IMAGE=eu.gcr.io/nextjournal-com/python:$TAG
-docker build -t nextjournal/python:$TAG .
-docker tag nextjournal/python:$TAG $GCR_IMAGE
+
 echo "Tagging $TAG"
 git tag $TAG
-echo "Pushing to $GCR_IMAGE..."
-docker push $GCR_IMAGE
-echo "Pushing to Github, which triggers an automated build for nextjournal/python:$TAG"
+
+echo "Pushing to Github, which triggers a Docker Hub automated build for nextjournal/python:$TAG"
 git push --tags
 git push
+
+docker tag nextjournal/python:$TAG $GCR_IMAGE
+echo "Pushing to $GCR_IMAGE..."
+docker push $GCR_IMAGE
